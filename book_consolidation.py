3e22076d7eba9a97c256d940d969aeb0cba49960
adamk99 <adam.kwong99@gmail.com>
2021-07-12 16:29:23 -0700

    sf_aliens move

--- book_consolidation.py
+++ book_consolidation.py
@@ -1,5 +1,11 @@
 # importing csv module
 import csv
+import os
+
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, files))
+  
   
 # csv file name
 filename = "sf_aliens.csv"
