552a30c3f355835f4e471fe5eea9f05be35b11d4
adamk99 <adam.kwong99@gmail.com>
2021-07-14 09:00:55 -0700

    update

--- book_consolidation.py
+++ book_consolidation.py
@@ -1,50 +1,55 @@
-# importing csv module
 import csv
 import os
 
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
-print("\n \n")
-  
-# needs to be integrated-------------
-# directory = os.path.join(cwd,"Sci-Fi Books")
-# for root,dirs,files in os.walk(directory):
-#     for file in files:
-#        if file.endswith(".csv"):
-#            f=open(file, 'r')
-#            #  perform calculation
-#            f.close()
+"""
+This first block gets current working directory into cwd. Then uses the cwd to join and make a new
+path using os.path.join for the "Sci-Fi Books" folder.  It then prints the current directory and
+lists all the files in the directory.
+"""
 
-# csv file name
-filename = "sf_aliens.csv"
+cwd = os.getcwd()  # Get the current working directory (cwd)
+print("\n \n", cwd)
+print("\n")
+directory = os.path.join(cwd, "Sci-Fi Books")
+listfiles = os.listdir(directory)  # Get all the files in that directory
+print("Files in %r: %s" % (directory, listfiles))
+print("\n \n \n")
+
+print(directory)
+
+for root,dirs,files in os.walk(directory):
+    for file in files:
+        if file.endswith('.csv'):
+            print('File name: ', file)
+            filepath=os.path.join(directory, file)
+            print('File path: ', filepath, '\n')
+            f = open(filepath, mode= 'r', encoding= 'utf-8') 
+            # initializing the titles and rows list
+            fields = []
+            rows = []
   
-# initializing the titles and rows list
-fields = []
-rows = []
-  
-# reading csv file
-with open(filename, mode= 'r', encoding= 'utf-8') as csvfile:
-    # creating a csv reader object
-    csvreader = csv.reader(csvfile)
+            #creating a csv reader object
+            csvreader = csv.reader(f)
       
-    # extracting field names through first row
-    fields = next(csvreader)
+            #extracting field names through first row
+            fields = next(csvreader)
   
-    # extracting each data row one by one
-    for row in csvreader:
-        rows.append(row)
+            #extracting each data row one by one
+            for row in csvreader:
+                rows.append(row)
   
-    # get total number of rows
-    print("Total no. of rows: %d"%(csvreader.line_num))
+            #get total number of rows
+            print("Total no. of rows: %d"%(csvreader.line_num))
   
-# printing the field names
-print('Field names are:' + ', '.join(field for field in fields))
+            #printing the field names
+            print('Field names are: ' + ', '.join(field for field in fields))
   
-#  printing first 5 rows
-print('\nFirst 5 rows are:\n')
-for row in rows[:5]:
-    # parsing each column of a row
-    for col in row:
-        print("%10s"%col),
-    print('\n')
+            #printing first 5 rows
+            print('\nFirst 5 rows are:\n')
+            for row in rows[:5]:
+                #parsing each column of a row
+                for col in row:
+                    print("%10s"%col),
+                print('\n')
+            print('-------------------- CSV FILE END --------------------\n\n')
+            f.close()
