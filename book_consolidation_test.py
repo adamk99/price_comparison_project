3730285166aa061d952daeaedc89f42416bd4cd0
adamk99 <adam.kwong99@gmail.com>
2021-07-14 09:30:25 -0700

    recursive loop to create csv file complete

--- book_consolidation_test.py
+++ book_consolidation_test.py
@@ -1,9 +1,11 @@
-# importing csv module
 import csv
 import os
 
-
-
+"""
+This first block gets current working directory into cwd. Then uses the cwd to join and make
+a new path using os.path.join for the "Sci-Fi Books" folder.  It then prints the current
+directory and lists all the files in the directory.
+"""
 
 cwd = os.getcwd()  # Get the current working directory (cwd)
 print("\n \n", cwd)
@@ -12,8 +14,37 @@ directory = os.path.join(cwd, "Sci-Fi Books")
 listfiles = os.listdir(directory)  # Get all the files in that directory
 print("Files in %r: %s" % (directory, listfiles))
 print("\n \n \n")
-
 print(directory)
+
+"""
+goal:
+Create NEW csv file.  If run again enumerate and create a new file. for ex. csv0, if csv0 is
+already created create csv1 and so on and so forth.
+
+Once created we will use existing read loop to edit at the same time organizing it line by
+line alphabetically.
+"""
+x = 0
+new_csv_path = os.path.join(cwd, "csv" + str(x))
+
+print(new_csv_path)
+
+while os.path.isfile(new_csv_path) == True:
+    x+=1
+    new_csv_path = os.path.join(cwd, 'csv' + str(x))
+    print(new_csv_path)
+    
+    
+newfile = open('csv' + str(x), mode = 'x')
+newfile.close()
+
+
+"""
+
+"""
+#explain for loop and additional functions
+
+"""
 
 for root,dirs,files in os.walk(directory):
     for file in files:
@@ -25,9 +56,6 @@ for root,dirs,files in os.walk(directory):
             # initializing the titles and rows list
             fields = []
             rows = []
-  
-            #reading csv file
-            #with open(filename, mode= 'r', encoding= 'utf-8') as csvfile:
 
             #creating a csv reader object
             csvreader = csv.reader(f)
@@ -46,11 +74,13 @@ for root,dirs,files in os.walk(directory):
             print('Field names are: ' + ', '.join(field for field in fields))
   
             #printing first 5 rows
-            print('\nFirst 5 rows are:\n')
-            for row in rows[:5]:
-                #parsing each column of a row
-                for col in row:
-                    print("%10s"%col),
-                print('\n')
-            print('-------------------- CSV FILE END --------------------\n\n')
+            # print('\nFirst 5 rows are:\n')
+            # for row in rows[:5]:
+            #     #parsing each column of a row
+            #     for col in row:
+            #         print("%10s"%col),
+            #     print('\n')
+            # print('-------------------- CSV FILE END --------------------\n\n')
             f.close()
+
+"""
